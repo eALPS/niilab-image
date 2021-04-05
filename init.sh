bcb70fe5f358a4f7c057eaed9aac06c1d6ff4c67
BuildTools <unconfigured@null.spigotmc.org>
2021-04-05 12:10:38 +0900

    インストール設定

--- init.sh
+++ init.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
+# スクリプトの場所に移動
+cd `dirname $0`
+#実行属性付与
+chmod -R +x ./module
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-# /etc/apt/sources.listは初回起動時にリセットを食らうので、.dに入れる
-#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo sh -c "cat << EOF >> /etc/apt/sources.list.d/docker.list
-deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable
-EOF"
+./module/docker_install.sh
+./module/openvswitch_install.sh
+./module/codeserver_install.sh
+./module/codeserver_setting.sh
+./module/container_setting.sh
 
 
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-sudo systemctl restart docker.service 
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-docker run --rm hello-world
+# tcpdumpの制限解除
+sudo apt install apparmor-utils -y
+sudo aa-complain /usr/sbin/tcpdump
