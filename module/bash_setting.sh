bb96c1442768fe97a916e8adc3c756a8e693a9fc
BuildTools <unconfigured@null.spigotmc.org>
2021-04-24 22:07:13 +0900

    tab補完の設定

--- module/bash_setting.sh
+++ module/bash_setting.sh
@@ -3,3 +3,11 @@ curl https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec
 echo "source ~/.bash-preexec.sh" | tee -a ~/.bashrc
 echo 'preexec() { logger -t code-server[ealplus] "$1 exec"; }' | tee -a ~/.bashrc
 
+cat <<EOF | tee -a ~/.bashrc
+if [ -f /etc/bash_completion ]; then
+  . /etc/bash_completion
+fi
+EOF
+
+
+
