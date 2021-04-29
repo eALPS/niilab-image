3a4dd9f28400e7c5d4c3cfb84ea2933902b6dc08
BuildTools <unconfigured@null.spigotmc.org>
2021-04-29 15:59:22 +0900

    docker-composeのバージョン更新

--- module/docker_install.sh
+++ module/docker_install.sh
@@ -31,5 +31,5 @@ COMMENTOUT
 
 sudo systemctl restart docker.service 
 # composeのインストール
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
